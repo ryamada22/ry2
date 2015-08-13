2e23c5aed231cf40bd65f800567a1943ee1cda7a
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2015-08-13 12:00:28 +0900

    initial load

---
+++ R/fx.R
@@ -0,0 +1,26 @@
+#' Dummy title
+#'
+#' Dummy description.
+#' @examples
+#' myfx1(10)
+#' myfx2(5)
+#' myfx4()
+
+#' @export
+myfx1 <- function(n){
+	runif(n)
+}
+#' @export
+myfx2 <- function(n){
+	rnorm(n)
+}
+
+myfx3 <- function(){
+	sample(1:100,1)
+}
+
+#' @export
+myfx4 <- function(){
+	n <- myfx3()
+	rnorm(n)
+}
